20f1885b52a51c97eccdc32a6cab2b834feb47ae
Kamal Shah <shahkamal2283@gmail.com>
2025-02-25 16:57:08 +0530

    enhance navbar component with styled dashboard link and animation

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -38,11 +38,19 @@ const Navbar = async () => {
         </ul>
       </nav>
       <aside className="flex items-center gap-4">
-        <Link href="dashboard"></Link>
+        <Link
+          href="/dashboard"
+          className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+        >
+          <span className="absolute inset-[-100%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            {true ? "Dashboard" : "Get Started"}
+          </span>
+        </Link>
       </aside>
     </header>
   );
 };
 
 export default Navbar;
-//19:13
+//22:55
