8764cc220d6f1cbd70981db18b08daf649237ce1
Kamal Shah <shahkamal2283@gmail.com>
2025-04-20 22:31:30 +0530

    feat: add Tabs component and integrate into EditorCanvasSidebar; enhance drag-and-drop functionality

--- src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas-sidebar.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas-sidebar.tsx
@@ -1,8 +1,20 @@
 ("");
-import { EditorNodeType } from "@/lib/types";
+import { EditorCanvasTypes, EditorNodeType } from "@/lib/types";
 import { useEditor } from "@/providers/editor-provider";
-import { useNodeConnections } from "@xyflow/react";
+import { useNodeConnections } from "@/providers/connections.provider";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 import React from "react";
+import { Separator } from "@/components/ui/separator";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import EditorCanvasIconHelper from "./editor-canvas-card-icon-helper";
+import { EditorCanvasDefaultCardTypes } from "@/lib/constant";
+import { onDragStart } from "@/lib/editor-utils";
 
 type Props = {
   nodes: EditorNodeType[];
@@ -10,8 +22,46 @@ type Props = {
 
 const EditorCanvasSidebar = ({ nodes }: Props) => {
   const { state } = useEditor();
-  const nodeConnection = useNodeConnections();
-  return <div>EditorCanvasSidebar</div>;
+  const { nodeConnection } = useNodeConnections();
+
+  return (
+    <aside>
+      <Tabs defaultValue="actions" className="h-screen overflow-scroll pb-24">
+        <TabsList className="bg-transparent">
+          <TabsTrigger value="actions">Actions</TabsTrigger>
+          <TabsTrigger value="settings">Settings</TabsTrigger>
+        </TabsList>
+        <Separator />
+        <TabsContent value="actions" className="flex flex-col gap-4 p-4">
+          {Object.entries(EditorCanvasDefaultCardTypes)
+            .filter(
+              ([_, cardType]) =>
+                (!nodes.length && cardType.type === "Trigger") ||
+                (nodes.length && cardType.type === "Action")
+            )
+            .map(([cardKey, cardValue]) => (
+              <Card
+                key={cardKey}
+                draggable
+                className="w-full cursor-grab border-black bg-neutral-100 dark:border-neutral-700 dark:bg-neutral-900"
+                onDragStart={(event) =>
+                  onDragStart(event, cardKey as EditorCanvasTypes)
+                }
+              >
+                <CardHeader className="flex flex-row items-center gap-4 p-4">
+                  <EditorCanvasIconHelper type={cardKey as EditorCanvasTypes} />
+                  <CardTitle className="text-md">
+                    {cardKey}
+                    <CardDescription>{cardValue.description}</CardDescription>
+                  </CardTitle>
+                </CardHeader>
+              </Card>
+            ))}
+        </TabsContent>
+      </Tabs>
+    </aside>
+  );
 };
 
 export default EditorCanvasSidebar;
+//4:09:43
---
+++ src/lib/editor-utils.ts
@@ -0,0 +1,10 @@
+import { Editor } from "@/providers/editor-provider";
+import { EditorCanvasCardType } from "./types";
+
+export const onDragStart = (
+  event: any,
+  nodeType: EditorCanvasCardType["type"]
+) => {
+  event.dataTransfer.setData("application/reactflow", nodeType);
+  event.dataTransfer.effectAllowed = "move";
+};
