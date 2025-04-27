195e1b5ba9d28a4268d4824222d7e767e61eed5b
Kamal Shah <shahkamal2283@gmail.com>
2025-04-27 15:58:44 +0530

    feat: implement content management for Slack, Discord, and Notion; add Google Drive file details and render output components

---
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/google-drive-files.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+type Props = {};
+
+const GoogleDriveFiles = (props: Props) => {
+  return <div>GoogleDriveFiles</div>;
+};
+
+export default GoogleDriveFiles;
+// 4:34:12
